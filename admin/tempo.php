2ebd9e6eab612557b7db6e58a6fdb1b3c087bcf0
TitiLb <131799791+TitiLb@users.noreply.github.com>
2024-11-30 17:51:39 +0100

    Update tempo.php

--- admin/tempo.php
+++ admin/tempo.php
@@ -1,2 +1,12 @@
 <?php
-echo realpath('tempo.php');
+echo "Fichier : chemin.php<br/>" ;
+echo "<br/>";
+ 
+echo "Le chemin relatif est :<br/>";
+echo realpath('chemin.php'); 
+echo "<br/>";
+echo "<br/>";
+ 
+echo "Le chemin absolu est :<br/>";
+echo $_SERVER['PHP_SELF'];
+?>
